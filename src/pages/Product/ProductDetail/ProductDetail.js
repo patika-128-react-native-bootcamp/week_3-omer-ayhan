1ff690cea884cb87d0ebc8eef2c1c77d2038b568
Ömer Ayhan <om.ayhan247@gmail.com>
2021-12-04 16:22:29 +0300

    fix: :adhesive_bandage: ProductDetail component fixes
    - Added Flatlist for listing object instead of map method
    - removed callbacks and changed them with named functions

--- src/pages/Product/ProductDetail/ProductDetail.js
+++ src/pages/Product/ProductDetail/ProductDetail.js
@@ -1,36 +1,48 @@
-import {useRoute} from '@react-navigation/core';
-import React from 'react';
-import {View, SafeAreaView, Text, ScrollView, Image} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import styles from './ProductDetail.styles';
+import { useRoute } from "@react-navigation/core";
+import React from "react";
+import {
+  View,
+  SafeAreaView,
+  Text,
+  ScrollView,
+  Image,
+  FlatList,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import styles from "./ProductDetail.styles";
 
 export default function ProductDetail() {
   const route = useRoute();
+  const { product } = route.params;
 
-  const {product} = route.params;
+  const image = {
+    uri: product.imageURL,
+  };
+
+  const extractId = (_, index) => `${product.p_id}_${index}`;
+
+  const renderIngredients = ({ item }) => (
+    <View style={styles.badge_container}>
+      <Text style={styles.badge_label}>{item}</Text>
+    </View>
+  );
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Image
-          style={styles.image}
-          source={{
-            uri: product.imageURL,
-          }}
-        />
+        <Image style={styles.image} source={image} />
         <View style={styles.name_container}>
           <Text style={styles.name_label}>{product.name}</Text>
           {product.isPopular && <Icon name="star" color="orange" size={25} />}
         </View>
-        <ScrollView horizontal bounces={false}>
-          {product.ingredients.map((ing, ind) => {
-            return (
-              <View style={styles.badge_container}>
-                <Text style={styles.badge_label}>{ing}</Text>
-              </View>
-            );
-          })}
-        </ScrollView>
+        <FlatList
+          horizontal
+          bounces={false}
+          keyExtractor={extractId}
+          data={product.ingredients}
+          renderItem={renderIngredients}
+        />
+
         <View style={styles.description_container}>
           <Text style={styles.description}>{product.description}</Text>
         </View>
