9f744e554955d4ebcfd77a21fb6ba128d15103e9
Ömer Ayhan <om.ayhan247@gmail.com>
2021-12-06 17:19:18 +0300

    refactor: :recycle: removed Formik
    removed Formik and used react states for form control

--- src/pages/Menu/CreateMenu/CreateMenu.js
+++ src/pages/Menu/CreateMenu/CreateMenu.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/core";
 import { Alert, SafeAreaView, ScrollView, Text } from "react-native";
-import { Formik } from "formik";
 
 import Input from "../../../components/Input";
 import Button from "../../../components/Button";
@@ -11,17 +10,14 @@ import routes from "../../../navigation/routes";
 
 export default function CreateMenu() {
   const navigation = useNavigation();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [ingredients, setIngredients] = useState("");
 
   const route = useRoute();
 
-  const initialValues = {
-    name: "",
-    description: "",
-    ingredients: "",
-    price: "",
-  };
-
-  function handleNavigateDetail({ name, description, ingredients, price }) {
+  function handleNavigateDetail() {
     if (!name || !description || !ingredients || !price) {
       Alert.alert("Error", "Fill all the fields");
       return;
@@ -43,36 +39,17 @@ export default function CreateMenu() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Formik initialValues={initialValues} onSubmit={handleNavigateDetail}>
-        {({ values, handleChange, handleSubmit }) => (
-          <ScrollView>
-            <Text style={styles.menu_name}>{route.params.menuName}</Text>
-            <Input
-              label="Name:"
-              value={values.name}
-              onChangeText={handleChange("name")}
-            />
-            <Input
-              label="Description:"
-              value={values.description}
-              onChangeText={handleChange("description")}
-              multiline
-            />
-            <Input
-              label="Ingredients(separate each one with a comma):"
-              value={values.ingredients}
-              onChangeText={handleChange("ingredients")}
-            />
-            <Input
-              label="Price:"
-              value={values.price}
-              onChangeText={handleChange("price")}
-              keyboardType="numeric"
-            />
-            <Button title="Apply Food" onPress={handleSubmit} />
-          </ScrollView>
-        )}
-      </Formik>
+      <ScrollView>
+        <Text style={styles.menu_name}>{route.params.menuName}</Text>
+        <Input label="Name:" onChangeText={setName} />
+        <Input label="Description:" onChangeText={setDescription} multiline />
+        <Input
+          label="Ingredients(separate each one with a comma):"
+          onChangeText={setIngredients}
+        />
+        <Input label="Price:" onChangeText={setPrice} keyboardType="numeric" />
+        <Button title="Apply Food" onPress={handleNavigateDetail} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
