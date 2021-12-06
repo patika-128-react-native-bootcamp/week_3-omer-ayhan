9168c35a634caf1615d73829cc36ef26d598368a
Ömer Ayhan <om.ayhan247@gmail.com>
2021-12-06 16:50:05 +0300

    fix: :bug: fixed file naming for ProductDetailCard

--- src/components/cards/ProductDetailCard/ProductDetailCard.js
+++ src/components/cards/ProductDetailCard/ProductDetailCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./ProductDetailCard.styles";
 
 export default function ProductDetailCard({ detailData }) {
   return (
